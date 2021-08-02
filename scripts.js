89f2573009fa62e8758941678af75085c37cc0a9
Kasper Mennink <kasper.mennink@hotmail.com>
2021-08-02 22:35:45 +0200

    Added a prompt and first bit of game function.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,20 @@
 function computerPlay() {
 return options[Math.floor(Math.random() * options.length)];
 }
-const options = ["Rock","Paper","Scissors"];
+const options = ["rock","paper","scissors"];
 console.log(computerPlay());
+
+let playerPick = prompt ("Choose rock, paper or scissors.").toLowerCase();
+
+const playerSelection = playerPick;
+const computerSelection = computerPlay();
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie.";
+    } else if ((computerSelection === "scissors" && playerSelection === "rock") || (computerSelection === "paper" && playerSelection === "scissors") || (computerSelection = "rock" && playerSelection === "paper")); {
+        return "You win."
+    }
+}
+
+console.log(playRound (playerSelection, computerSelection));
