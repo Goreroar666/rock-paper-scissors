e09081721311899a425000039177073e2c38e51a
Kasper Mennink <kasper.mennink@hotmail.com>
2021-08-03 22:42:02 +0200

    Added a playround function.

--- scripts.js
+++ scripts.js
@@ -4,17 +4,38 @@ return options[Math.floor(Math.random() * options.length)];
 const options = ["rock","paper","scissors"];
 console.log(computerPlay());
 
-let playerPick = prompt ("Choose rock, paper or scissors.").toLowerCase();
+const playerPick = prompt("Choose rock, paper or scissors.").toLowerCase();
 
 const playerSelection = playerPick;
 const computerSelection = computerPlay();
 
-function playRound (playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "rock") {
         return "It's a tie.";
-    } else if ((computerSelection === "scissors" && playerSelection === "rock") || (computerSelection === "paper" && playerSelection === "scissors") || (computerSelection = "rock" && playerSelection === "paper")); {
-        return "You win."
+    } if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win, rock beats scissors.";
+    } if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose, paper beats rock.";
+} if (playerSelection === "scissors" && computerSelection === "scissors") {
+    return "It's a tie.";
+} if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You win, scissors beats paper.";
+} if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "You lose, rock beats scissors.";
+} if (playerSelection === "paper" && computerSelection === "paper") {
+    return "It's a tie.";
+} if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win, paper beats rock.";
+} if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You lose, scissors beats paper.";}
+}
+function game (playerSelection) {
+    for (i = 0; i < 5; i++) {
+        playRound(playerSelection, computerSelection);
     }
 }
+let computerScore = 0;
+let playerScore = 0;
 
-console.log(playRound (playerSelection, computerSelection));
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
