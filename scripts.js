095a09d852f73bb1a372ec5aae6e64bf71fa7921
Kasper Mennink <kasper.mennink@hotmail.com>
2021-08-04 20:31:18 +0200

    Got a working solution for the console version going.

--- scripts.js
+++ scripts.js
@@ -2,40 +2,52 @@ function computerPlay() {
 return options[Math.floor(Math.random() * options.length)];
 }
 const options = ["rock","paper","scissors"];
-console.log(computerPlay());
 
-const playerPick = prompt("Choose rock, paper or scissors.").toLowerCase();
+let playerSelection = prompt("Please choose rock, paper or scissors.").toLowerCase();
+let computerSelection = computerPlay();
 
-const playerSelection = playerPick;
-const computerSelection = computerPlay();
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock" && computerSelection === "rock") {
-        return "It's a tie.";
-    } if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You win, rock beats scissors.";
-    } if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You lose, paper beats rock.";
-} if (playerSelection === "scissors" && computerSelection === "scissors") {
-    return "It's a tie.";
-} if (playerSelection === "scissors" && computerSelection === "paper") {
-    return "You win, scissors beats paper.";
-} if (playerSelection === "scissors" && computerSelection === "rock") {
-    return "You lose, rock beats scissors.";
-} if (playerSelection === "paper" && computerSelection === "paper") {
-    return "It's a tie.";
-} if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You win, paper beats rock.";
-} if (playerSelection === "paper" && computerSelection === "scissors") {
-    return "You lose, scissors beats paper.";}
-}
-function game (playerSelection) {
-    for (i = 0; i < 5; i++) {
-        playRound(playerSelection, computerSelection);
-    }
-}
 let computerScore = 0;
 let playerScore = 0;
 
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie.";
+    } if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore ++;
+        return "You win, rock beats scissors.";
+    } if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore ++;
+        return "You lose, paper beats rock.";
+} if (playerSelection === "scissors" && computerSelection === "paper") {
+    playerScore ++;
+    return "You win, scissors beats paper.";
+} if (playerSelection === "scissors" && computerSelection === "rock") {
+    computerScore ++;
+    return "You lose, rock beats scissors.";
+} if (playerSelection === "paper" && computerSelection === "rock") {
+    playerScore ++;
+        return "You win, paper beats rock.";
+} if (playerSelection === "paper" && computerSelection === "scissors") {
+    computerScore ++;
+    return "You lose, scissors beats paper.";}
+}
+
+function game () {
+    for (i = 0; i < 5; i++) {
+        let playerSelection = prompt("Please choose rock, paper or scissors.").toLowerCase();
+        let computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log ("Score:", playerScore, "-", computerScore);
+        function result() {
+            if (playerScore > computerScore) {
+                return "You won!"
+            } else if (computerScore > playerScore) {
+                return "You lost, the computer won!"
+            } else {
+                return "It is a tie."
+            }
+
+        }
+    }
+    console.log(result());
+}
